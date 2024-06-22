567e13cfd45a5d3fa115dc2095ca3d31bdb91cbf
013thedaniel <dzuban.daniil@gmail.com>
2024-06-23 00:59:45 +0300

    lab5

---
+++ main.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class NeuralNetwork:
+    def __init__(self):
+        self.W1 = np.random.normal(size=(9, 784)) * np.sqrt(1 / 784)
+        self.B1 = np.random.normal(size=(9, 1)) * np.sqrt(1 / 9)
+        self.W2 = np.random.normal(size=(3, 9)) * np.sqrt(1 / 9)
+        self.B2 = np.random.normal(size=(3, 1)) * np.sqrt(1 / 3)
+
+    @staticmethod
+    def __relu(Z):
+        return np.maximum(Z, 0)
+
+    @staticmethod
+    def __relu_derivative(Z):
+        return (Z > 0).astype(int)
+
+    @staticmethod
+    def __softmax(Z):
+        exps = np.exp(Z)
+        return exps / np.sum(exps, axis=0)
+
+    def __forward_propagation(self, X):
+        Z1 = self.W1 @ X + self.B1
+        A1 = self.__relu(Z1)
+        Z2 = self.W2 @ A1 + self.B2
+        A2 = self.__softmax(Z2)
+        return Z1, A1, Z2, A2
+
+    def __backward_propagation(self, X, Z1, A1, A2, A2E):
+        num_of_training_sets = A2E.shape[1]
+        dZ2 = A2 - A2E
+        dW2 = (dZ2 @ A1.T) / num_of_training_sets
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / num_of_training_sets
+        dZ1 = self.W2.T @ dZ2 * self.__relu_derivative(Z1)
+        dW1 = (dZ1 @ X.T) / num_of_training_sets
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / num_of_training_sets
+        return dW1, db1, dW2, db2
+
+    def __update_weights(self, dW1, dB1, dW2, dB2, alpha):
+        self.W1 = self.W1 - alpha * dW1
+        self.B1 = self.B1 - alpha * dB1
+        self.W2 = self.W2 - alpha * dW2
+        self.B2 = self.B2 - alpha * dB2
+
+    def __get_sequence_of_expected_prediction(self, Y):
+        sequence = np.zeros((Y.size, self.B2.size))
+        sequence[np.arange(Y.size), Y - 1] = 1
+        return sequence.T
+
+    def train(self, X, Y, alpha, iterations):
+        A2E = self.__get_sequence_of_expected_prediction(Y)
+        for i in range(iterations):
+            Z1, A1, Z2, A2 = self.__forward_propagation(X)
+            dW1, dB1, dW2, dB2 = self.__backward_propagation(X, Z1, A1, A2, A2E)
+            self.__update_weights(dW1, dB1, dW2, dB2, alpha)
+
+    def test_nn_accuracy(self, X, Y):
+        _, _, _, A2 = self.__forward_propagation(X)
+        predictions = np.argmax(A2, 0) + 1
+        return np.sum(predictions == Y) / Y.size, predictions[:10]
+
+
+def test_prediction(X, index):
+    current_image = X[:, index, None]
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('data.csv').to_numpy()
+    np.random.shuffle(data)
+
+    training_set = data[:-1000].T
+    Y_training, X_training = np.split(training_set, [1])
+    testing_set = data[-1000:].T
+    Y_testing, X_testing = np.split(testing_set, [1])
+
+    nn = NeuralNetwork()
+
+    print(f'\nЧисла на зображеннях: {", ".join(list(map(str, Y_testing[0][:10])))}.')
+    for i in range(10):
+        test_prediction(X_testing, i)
+
+    nn_accuracy, predicted_nums = nn.test_nn_accuracy(X_testing / 255., Y_testing)
+    print(f'\n\tРозпізнані значення до навчання: {", ".join(list(map(str, predicted_nums)))}')
+    print(f'\tТочність розпізнавання чисел до навчання: {round(nn_accuracy * 100, 1)}%')
+
+    nn.train(X_training / 255., Y_training, 0.1, 250)
+
+    nn_accuracy, predicted_nums = nn.test_nn_accuracy(X_testing / 255., Y_testing)
+    print(f'\n\tРозпізнані значення після навчання: {", ".join(list(map(str, predicted_nums)))}')
+    print(f'\tТочність розпізнавання чисел після навчання: {round(nn_accuracy * 100, 1)}%')
